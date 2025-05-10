e8e817c6f34ff393b45fe08a35dc8118914d6959
Hemant Kumar Sharma <137736113+Hps7783@users.noreply.github.com>
2025-05-10 14:17:27 +0530

    Length of array

---
+++ LengthOfArray.java
@@ -0,0 +1,11 @@
+// Find the length Of Array
+
+public class LengthOfArray {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5,6,7,8,9,0};
+        // Logic
+        int LOfArray=a.length;
+        System.out.println("the Array Length is "+ LOfArray);
+    }
+    
+}
